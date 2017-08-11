44c396d616434ed8f450348b0f54791360f6e63a
ally <allisterke@gmail.com>
2017-08-11 22:00:11 +0800

    first commit

---
+++ wp-content/themes/twentyfifteen/footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the "site-content" div and all content after.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+?>
+
+	</div><!-- .site-content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo" style="display:flex">
+
+		<div style="margin:0 auto; padding:20px 0;">
+			<a target="_blank" href="http://www.miitbeian.gov.cn/" rel="nofollow" title="工业和信息化部ICP/IP地址/域名信息备案管理系统" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;">
+				<p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">浙ICP备17032656号</p>
+			</a>
+			<a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33010602008454" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;">
+				<img src="/beian-icon.png" style="float:left;"/>
+				<p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">浙公网安备 33010602008454号</p>
+			</a>
+		</div>
+		 
+	</footer><!-- .site-footer -->
+
+</div><!-- .site -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
